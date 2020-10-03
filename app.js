ffd90f0aed8eaa864418f4180b2ecda9ea10b8b4
shinfujita <shin4522@gmail.com>
2020-10-03 16:30:24 +0900

    master-2020でコミット

--- app.js
+++ app.js
@@ -1,52 +1,16 @@
 'use strict';
-// イベント駆動型プログラミング
-const fs = require('fs'); // FileSystem、ファイルを扱うモジュール
-const readline = require('readline'); // ファイルを一行ずつ読み込むためのモジュール
+const fs = require('fs');
+const readline = require('readline');
 const rs = fs.createReadStream('./popu-pref.csv');
-const rl = readline.createInterface({ 'input': rs, 'output': {} });
-const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
-/* rl.on('line', (lineString) => {
-  console.log(lineString);
-}); */
-rl.on('line', (lineString) => { // lineStringは文字列の意味、つまりここで文字列を出力している
-    const columns = lineString.split(','); // 文字列をカンマで区切って配列にする
-    const year = parseInt(columns[0]); // 文字列を数値として扱えるようにparseInt関数を使用
-    const prefecture = columns[1];
-    const popu = parseInt(columns[3]); // 文字列を数値として扱えるようにparseInt関数を使用
-    if (year === 2010 || year === 2015) { // 2010年と2015年のデータのみ表示（最初の行は非表示）
-      /* console.log(year);
-      console.log(prefecture);
-      console.log(popu); */
-      let value = prefectureDataMap.get(prefecture);
-      if (!value) { // データが取得できなかったら以下のオブジェクトにデータを入れる
-        value = {
-          popu10: 0,
-          popu15: 0,
-          change: null
-        };
-      }
-      if (year === 2010) {
-        value.popu10 = popu;
-      }
-      if (year === 2015) {
-        value.popu15 = popu;
-      }
-      prefectureDataMap.set(prefecture, value);
-      // console.log(value);
+const rl = readline.createInterface({ input: rs, output: {} });
+rl.on('line', lineString => {
+  const columns = lineString.split(',');
+  const year = parseInt(columns[0]);
+  const prefecture = columns[1];
+  const popu = parseInt(columns[3]);
+  if (year === 2010 || year === 2015) {
+    console.log(year);
+    console.log(prefecture);
+    console.log(popu);
   }
-  });
-  rl.on('close', () => {
-    /* console.log(prefectureDataMap); */
-    for (let [key,value] of prefectureDataMap) {
-      value.change = value.popu15 / value.popu10;
-    }
-    const rankingArray = Array.from(prefectureDataMap).sort((pair1,pair2) => {
-      return pair1[1].change - pair2[1].change;
-    });
-    const rankingString = rankingArray.map(([key,value],i) => {
-        // return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
-        return `${i + 1}位 ${key}: ${value.popu10} -> ${value.popu15} 変化率: ${value.change}`;
-    });
-    console.log(rankingString);
-    // console.log(rankingString[0]);
-  });
+});
